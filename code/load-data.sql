5bf2041be39657636639d0a56f19b0df499af5e3
Tymur <antonovskiytimyr@gmail.com>
2020-11-28 12:46:22 +0100

    adding 2 table

--- code/load-data.sql
+++ code/load-data.sql
@@ -43,3 +43,27 @@ IGNORE 1 LINES
 (`Record ID`, `Agency Code`, `Agency Name`, `Agency TYPE`, `City`, `State`, `YEAR`, `MONTH`, `Incident`, 
 `Crime TYPE`, `Crime Solved`, `Victim Sex`, `Victim Age`, `Victim Race`, `Victim Ethnicity`, `Perpetrator Sex`, 
 `Perpetrator Age`, `Perpetrator Race`, `Perpetrator Ethnicity`, `Relationship`, `Weapon`, `Victim Count`, `Perpetrator Count`, `Record Source`);
+
+
+
+
+CREATE TABLE deaths(
+`State` VARCHAR(50) NOT NULL,
+`YEAR` YEAR NOT NULL,
+`Deaths` VARCHAR(50) NOT NULL,
+`Population` INTEGER NOT NULL,
+`Crude Rate` VARCHAR(50) NOT NULL,
+ `Crude Rate Lower 95% Confidence Interval` VARCHAR(50) NOT NULL,
+ `Crude Rate Upper 95% Confidence Interval` VARCHAR(50) NOT NULL,
+ `Prescriptions Dispensed by US Retailers in that year (millions)` INTEGER NOT NULL); 
+
+
+-- load table
+LOAD DATA INFILE 'c:/Program Files/MySQL/Uploads/opioiddeaths.csv'
+INTO TABLE deaths
+FIELDS TERMINATED BY ',' 
+LINES TERMINATED BY '\n' 
+IGNORE 1 LINES;
+
+
+ 
